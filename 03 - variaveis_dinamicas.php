4b88ff0cd0b16f2f800d764dc05bcb52edf01035
gustavo <gustavocosta060@gmail.com>
2020-07-26 15:56:37 -0300

    Create 03 - variaveis_dinamicas.php
    Aula 03

---
+++ 03 - variaveis_dinamicas.php
@@ -0,0 +1,14 @@
+<?php
+    //Variáveis Dinâmicas
+
+    $bebida = "refrigerante";
+    $$bebida = "Guaraná"; //variável terá o nome do valor da variável $bebida
+
+    echo "refrigerante";
+
+    $destino = "Cidade";
+    $$destino = "Ilhéus";
+
+    echo "<br> $Cidade"
+
+?>
