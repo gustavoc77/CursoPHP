4f578185f6d3eb76011fc1061fad3cc02df87f8f
gustavo <gustavocosta060@gmail.com>
2020-07-26 15:56:30 -0300

    Create 02 - variaveis.php
    Aula 02

---
+++ 02 - variaveis.php
@@ -0,0 +1,23 @@
+<?php
+    //Criando Variáveis
+    $nome = "Gustavo Costa";
+    $idade = 19;
+    $altura = 1.78;
+
+    echo "Meu nome é $nome, e tenho $idade anos. Minha altura é de $altura <br><hr>";
+
+    //Alterando valor de variáveis
+
+    $nome = "Felipe";
+    $idade = 29;
+    $altura = 1.90;
+
+    echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura <br>";
+
+    //As variáveis são Case Sensitive
+    $Carro = "Hilux";//será exibida
+    $carro = "Santa Fé";//outra variável que não será exibida
+
+    echo "$Carro";
+
+?>
