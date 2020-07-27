c1505b484bd586d98b9e679b32f097cfece9dd5b
gustavo <gustavocosta060@gmail.com>
2020-07-26 23:55:45 -0300

    Create 05- aspas_concatenacao.php
    Aspas e Concatenação

---
+++ 05- aspas_concatenacao.php
@@ -0,0 +1,9 @@
+<?php
+    $nome = 'Gustavo da Costa'; //aspas simples são literais(não são interpretadas)
+    echo $nome. "<hr>";
+    echo 'Meu nome é ' .$nome. ' e tenho 19 anos <br>'; //utiliza-se o ponto para concatenação com as aspas simples
+    echo 'Meu nome é ' .$nome. ' e tenho \'19\' anos <hr>'; 
+
+    echo "Meu nome é $nome <br>"; //aspas duplas interpretam as variáveis
+    echo "Meu nome é \'$nome\'";
+    ?>
